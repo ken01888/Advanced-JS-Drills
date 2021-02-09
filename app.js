2d5bdb0be1fd89234c419c1b75a3d346106b0f47
ken01888 <athena201017@gmail.com>
2021-02-09 11:15:48 -0600

    Advanced Js Drills

---
+++ app.js
@@ -0,0 +1,67 @@
+// console.log('Hello')
+// let avg = findAvg(2,2);
+// console.log('World',avg)
+
+// function findAvg(a,b){
+//     console.log(a,b)
+//     var answer=(a+b)/2;
+//     return answer; 
+// }
+
+// let fruits = ['apples','oranges','blueberries'];
+
+
+// function printFruit(){
+//     let leastFav;   
+//     console.log(leastFav)
+//     let favFruit = fruits[2];
+//     console.log(fruits[0])
+    
+//     function printFavFruit(){
+//         console.log(favFruit)
+//         leastFav = 'Durian';
+// } 
+// printFavFruit()
+// };
+
+// printFruit()
+//myFavFruit();
+
+
+helloName()
+
+function helloName(){
+console.log("Hello");
+let myName = {firstName:'Kenneth',
+lastName:"Cunningham"};
+console.log(myName.firstName+' '+myName.lastName)
+}
+
+let alertFunction = function (){
+    alert('Advanced JS Drills')
+}
+
+alertFunction()
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
